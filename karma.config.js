e23b3f857f177af04ceda33128ec6355d7f91d90
Brady Wied <brady@wied.us>
2014-10-31 18:14:55 -0500

    Better example of how to chain

--- karma.config.js
+++ karma.config.js
@@ -1,7 +1,7 @@
 module.exports = function(config) {
     config.set({
         preprocessors: {
-            '**/*.cjsx': ['react-coffee']
+            '**/*.cjsx': ['react-coffee', 'coffee']
         },
         
         reactCoffeePreprocessor: {
